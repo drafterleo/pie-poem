94feed8b18eb1d1c1b3b1bc76ffebfd2c2663b0f
drafterleo <drafterleo@gmail.com>
2016-01-11 14:10:57 +0400

    firs commit

---
+++ snicks.py
@@ -0,0 +1,17 @@
+# http://www.codeisart.ru/blog/python-shingles-algorithm/
+
+stop_words = (u'это', u'как', u'так',
+              u'и', u'в', u'над',
+              u'к', u'до', u'не',
+              u'на', u'но', u'за',
+              u'то', u'с', u'ли',
+              u'а', u'во', u'от',
+              u'со', u'для', u'о',
+              u'же', u'ну', u'вы',
+              u'бы', u'что', u'кто',
+              u'он', u'она')
+
+
+
+
+  
