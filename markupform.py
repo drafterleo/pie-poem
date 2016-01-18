c2200b066b2259edd43981e7080e8a642e3e00b8
drafterleo <drafterleo@gmail.com>
2016-01-18 17:58:21 +0400

    from work 18.01.2016

--- markupform.py
+++ markupform.py
@@ -3,9 +3,12 @@
 import sys
 from PyQt4 import QtGui
 from ui_markupform import Ui_MarkupForm
+import data_model as dm
 
 
 class Window(QtGui.QWidget, Ui_MarkupForm):
+    pmodel = {}
+    curr_idx = 0
 
     def __init__(self):
         super(Window, self).__init__()
@@ -13,6 +16,46 @@ class Window(QtGui.QWidget, Ui_MarkupForm):
         self.setupUi(self)
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("Markup")
+
+        self.loadModelBtn.clicked.connect(self.loadModel)
+        self.poemNumSpin.valueChanged[str].connect(self.poemSpinValueChanged)
+
+    def poemCount(self):
+        return len(self.pmodel['poems'])
+
+    def poemByIdx(self, idx):
+        return self.pmodel['poems'][idx]
+
+    def rateByIdx(self, idx):
+        return self.pmodel['rate'][idx]
+
+    def setPoemByIdx(self, idx):
+        self.poemLabel.setText(self.poemByIdx(idx))
+
+    def loadModel(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', '')
+        self.modelFileNameLabel.setText(fname)
+        self.pmodel = dm.read_data_model(fname)
+        if not ('rate' in self.pmodel.keys()):
+            self.pmodel['rate'] = [0.0 for _ in range(self.poemCount())]
+        self.curr_idx = 0
+        self.poemNumSpin.setMinimum(1)
+        self.poemNumSpin.setMaximum(self.poemCount())
+        self.poemNumSpin.setValue(1)
+
+    def saveModel(self):
+        return
+
+    def poemSpinValueChanged(self, val):
+        self.pmodel['rate'][self.curr_idx] = self.rateSlider.value() / self.rateSlider.maximum()
+        self.curr_idx = self.poemNumSpin.value() - 1
+        self.setPoemByIdx(self.curr_idx)
+        self.rateSlider.setValue(self.rateByIdx(self.curr_idx) * self.rateSlider.maximum())
+
+    def nextPoem(self):
+        return
+    def prevPoem(self):
+        return
 
 
 def main():
