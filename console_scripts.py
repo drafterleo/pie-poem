2c6b1f526eb713f197ac8ba23fc570eb9620c7ca
drafterleo <drafterleo@gmail.com>
2016-01-31 19:38:20 +0300

    minor fixes

--- console_scripts.py
+++ console_scripts.py
@@ -5,6 +5,7 @@ from pprint import pprint
 
 w2v = sem.load_w2v_model("c:/temp/data/ruscorpora.model.bin.gz")
 pm = dm.read_data_model("poems_model.dat")
-pprint(ap.similar_poems("запрос", pm, w2v))
+pprint(ap.similar_poems("запрос", pm, w2v, topn=5, use_associations=False))
 
 w2v.similarity("принцесса_S", "чудовище_S")
+w2v.most_similar(positive=['латифундия_S'])
