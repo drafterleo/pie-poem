316ef0fbfd3296015d9bb71a35d6a9de3b67111e
Drafterleo <drafterleo@gmail.com>
2017-04-18 17:24:59 +0400

    web service: setup_static dir

--- web_service.py
+++ web_service.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import os
 
 import semantics as sem
 import make_poems_model as mpm
@@ -19,17 +20,21 @@ def setup_routes(app):
     app.router.add_route('GET', '/', index)
     app.router.add_route('POST', '/poems', poems)
 
+def setup_static(app):
+    path = os.path.join(os.path.dirname(__file__), 'static')
+    app.router.add_static('/static/', path, name='static')
+
 
 # w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
 # pm = mpm.load_poems_model("poems_model.dat", w2v, vectorize=True)
 
 app = web.Application()
+setup_static(app)
 setup_routes(app)
 web.run_app(app, host='127.0.0.1', port=8081)
 
-# curl -X localhost:8081
-# curl -X POST -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" --data-ascii "words=%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81" localhost:8081
-# curl -X POST -d "words=%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81" localhost:8081
+
+# curl -X POST -d "words=%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81" localhost:8081/poems
 # curl -X POST --data-urlencode "words=запрос" localhost:8081
 
 
