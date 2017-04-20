7e6333ecf3474fe7faf84013b27b4e79d140b3f0
Drafterleo <drafterleo@gmail.com>
2017-04-20 13:39:05 +0400

    web service: as class

--- web_service.py
+++ web_service.py
@@ -4,45 +4,51 @@ import os
 import semantics as sem
 import make_poems_model as mpm
 
-async def index(request):
-    print(request)
-    return web.FileResponse('./static/index.html')
-
-
-async def poems(request):
-    print(request)
-    data = await request.post()
-    print(data['words'])
-    return web.Response(text=data['words'])
-
-
-def setup_routes(app):
-    app.router.add_route('GET', '/', index)
-    app.router.add_route('POST', '/poems', poems)
-
-
-def setup_static(app):
-    path = os.path.dirname(os.path.realpath(__file__)) + '/static/'
-    print(__file__, path)
-    app.router.add_static('/css', path + 'css')
-
-
-def load_models():
-    w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
-    pm = mpm.load_poems_model("poems_model.dat", w2v, vectorize=True)
-
-
 # curl -X POST -d "words=%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81" localhost:8081/poems
 # curl -X POST --data-urlencode "words=запрос" localhost:8081
-def start_web_server():
-    # load_models()
-    app = web.Application()
-    setup_static(app)
-    setup_routes(app)
-    web.run_app(app, host='127.0.0.1', port=8081)
+
+class PoemsWebServer:
+
+    def __init__(self):
+
+        # init web app
+        self.host = '127.0.0.1'
+        self.port = 8081
+        self.app = web.Application()
+        self.setup_static()
+        self.setup_routes()
+
+        # load models
+        # self.w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
+        # self.pm = mpm.load_poems_model("poems_model.dat", w2v, vectorize=True)
+
+    def setup_routes(self):
+        self.app.router.add_route('GET', '/', self.index)
+        self.app.router.add_route('POST', '/poems', self.poems)
+
+    def setup_static(self):
+        path = os.path.dirname(os.path.realpath(__file__)) + '/static/'
+        print(__file__, path)
+        self.app.router.add_static('/css', path + 'css')
+
+    # routed methods
+
+    async def index(self, request):
+        print(request)
+        return web.FileResponse('./static/index.html')
+
+    async def poems(self, request):
+        print(request)
+        data = await request.post()
+        print(data['words'])
+        return web.Response(text=data['words'])
+
+    def start_web_server(self):
+        web.run_app(self.app, host=self.host, port=self.port)
 
 
-start_web_server()
+web_server = PoemsWebServer()
+web_server.start_web_server()
 
 
 
