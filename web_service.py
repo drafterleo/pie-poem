c600c317dde283844f6eb46fe0052a8ad53cfb45
Drafterleo <drafterleo@gmail.com>
2017-04-20 15:12:15 +0400

    web service: poem response

--- web_service.py
+++ web_service.py
@@ -1,8 +1,10 @@
 from aiohttp import web
 import os
+import json
 
 import semantics as sem
 import make_poems_model as mpm
+import analyze_poem as ap
 
 # curl -X POST -d "words=%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81" localhost:8081/poems
 # curl -X POST --data-urlencode "words=запрос" localhost:8081
@@ -19,8 +21,8 @@ class PoemsWebServer:
         self.setup_routes()
 
         # load models
-        # self.w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
-        # self.pm = mpm.load_poems_model("poems_model.dat", w2v, vectorize=True)
+        self.w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
+        self.pm = mpm.load_poems_model("poems_model.dat", self.w2v, vectorize=True)
 
     def setup_routes(self):
         self.app.router.add_route('GET', '/', self.index)
@@ -28,8 +30,11 @@ class PoemsWebServer:
 
     def setup_static(self):
         path = os.path.dirname(os.path.realpath(__file__)) + '/static/'
-        print(__file__, path)
+        # print(__file__, path)
         self.app.router.add_static('/css', path + 'css')
+
+    def start_web_server(self):
+        web.run_app(self.app, host=self.host, port=self.port)
 
     # routed methods
 
@@ -39,16 +44,22 @@ class PoemsWebServer:
 
     async def poems(self, request):
         print(request)
-        data = await request.post()
-        print(data['words'])
-        return web.Response(text=data['words'])
+        data = await request.post()  # {name: val, ...}
+        words = data.get('words', '')
+        if len(words) > 0:
+            print(words)
+            sim_poems = ap.similar_poems(words, self.pm, self.w2v, topn=5, use_associations=False)  # -> [(p, s) ...]
+            poems = [spoem[0].replace('\n', '<br>') for spoem in sim_poems]
+            print(poems)
+            poems_json = json.dumps(poems, separators=(',', ':'), ensure_ascii=False)
+            return web.Response(text=poems_json)
+        else:
+            return web.Response(text='[]')
 
-    def start_web_server(self):
-        web.run_app(self.app, host=self.host, port=self.port)
 
-
-web_server = PoemsWebServer()
-web_server.start_web_server()
+if __name__ == "__main__":
+    web_server = PoemsWebServer()
+    web_server.start_web_server()
 
 
 
