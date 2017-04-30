aabee1d496d71448dcc124625879abf63918aa09
drafterleo <drafterleo@gmail.com>
2017-05-01 01:27:09 +0300

    404 added

--- web_service.py
+++ web_service.py
@@ -8,71 +8,87 @@ import semantics as sem
 import make_poems_model as mpm
 import analyze_poem as ap
 
+from typing import Callable
+
 # curl -X POST -d "words=%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81" localhost:8081/poems
 # curl -X POST --data-urlencode "words=запрос" localhost:8081
 
-class PoemsWebServer:
-
-    def __init__(self):
-
-        # init web app
-        self.host = '0.0.0.0'
-        self.port = 8081
-        self.app = web.Application()
-        self.setup_static()
-        self.setup_routes()
-
-        # load models
-        if sys.platform.startswith("win"):
-            self.w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
-        else:
-            self.w2v = sem.load_w2v_model("/data/ruscorpora.model.bin.gz")
-        self.pm = mpm.load_poems_model("poems_model_big.dat", self.w2v, vectorize=True)
-
-    def setup_routes(self):
-        # Configure default CORS (Cross-Origin Resource Sharing) settings.
-        cors = aiohttp_cors.setup(self.app, defaults={
-            "http://127.0.0.1": aiohttp_cors.ResourceOptions(
-                allow_credentials=True,
-                expose_headers="*",
-                allow_headers="*",
-            )
-        })
-
-        self.app.router.add_route('GET', '/', self.index)
-        cors.add(self.app.router.add_route('POST', '/poems', self.poems))
-
-    def setup_static(self):
-        path = os.path.dirname(os.path.realpath(__file__)) + '/static/'
-        # print(__file__, path)
-        self.app.router.add_static('/dist', path + 'dist')
-        self.app.router.add_static('/css', path + 'css')
+w2v = None
+pm = None
 
 
-    def start_web_server(self):
-        web.run_app(self.app, host=self.host, port=self.port)
+def load_models():
+    global w2v, pm
+    if sys.platform.startswith("win"):
+        w2v = sem.load_w2v_model("c:/data/ruscorpora.model.bin.gz")
+    else:
+        w2v = sem.load_w2v_model("/data/ruscorpora.model.bin.gz")
+    pm = mpm.load_poems_model("poems_model_big.dat", w2v, vectorize=True)
 
 
-    async def index(self, request):
-        return web.FileResponse('./static/index.html')
+def setup_routes(app: web.Application):
+    # Configure default CORS (Cross-Origin Resource Sharing) settings.
+    cors = aiohttp_cors.setup(app, defaults={
+        "http://127.0.0.1": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
 
-    async def poems(self, request):
-        data = await request.json()  # <- {name: val, ...}
-        words = data.get('words', '')
-        print('words: ', words)
-        if len(words) > 0:
-            sim_poems = ap.similar_poems(words[:50], self.pm, self.w2v, topn=10, use_associations=False)  # <- [(p, s) ...]
-            poems = [spoem[0].replace('\n', '<br>') for spoem in sim_poems]
-            print(poems)
-            poems_json = json.dumps(poems, separators=(',', ':'), ensure_ascii=False)
-            return web.Response(text=poems_json)
-        else:
-            return web.Response(text=json.dumps('[]'))
+    app.router.add_route('GET', '/', index)
+    cors.add(app.router.add_route('POST', '/poems', poems))
+
+
+def setup_static(app: web.Application):
+    path = os.path.dirname(os.path.realpath(__file__)) + '/static/'
+    # print(__file__, path)
+    app.router.add_static('/dist', path + 'dist')
+    app.router.add_static('/css', path + 'css')
+
+
+async def index(request):
+    return web.FileResponse('./static/index.html')
+
+
+async def poems(request):
+    data = await request.json()  # <- {name: val, ...}
+    words = data.get('words', '')
+    print('words: ', words)
+    if len(words) > 0:
+        sim_poems_dict = ap.similar_poems(words[:50], pm, w2v, topn=10, use_associations=False)  # <- [(p, s) ...]
+        sim_poems = [spoem[0].replace('\n', '<br>') for spoem in sim_poems_dict]
+        print(sim_poems)
+        poems_json = json.dumps(sim_poems, separators=(',', ':'), ensure_ascii=False)
+        return web.Response(text=poems_json)
+    else:
+        return web.Response(text=json.dumps('[]'))
+
+
+async def error_middleware(app: web.Application, handler: Callable) -> Callable:
+    async def middleware_handler(request):
+        try:
+            response = await handler(request=request)
+            if response.status == 404:
+                return web.FileResponse('./static/404.html')
+            return response
+        except web.HTTPException as ex:
+            if ex.status == 404:
+                return web.FileResponse('./static/404.html')
+            raise
+    return middleware_handler
+
+
+def start_web_server():
+    app = web.Application(middlewares=[error_middleware])
+    load_models()
+    setup_routes(app)
+    setup_static(app)
+    web.run_app(app, host='0.0.0.0', port=8081)
 
 
 if __name__ == "__main__":
-    web_server = PoemsWebServer()
-    web_server.start_web_server()
+    start_web_server()
 
 
 
